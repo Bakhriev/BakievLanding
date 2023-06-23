3076c55b4fef18d82167a12f766729ab13371970
Bakhriev <legendofpain5112@gmail.com>
2023-06-23 17:12:50 +0500

    xd

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -134,3 +134,23 @@ window.onpointermove = event => {
     { duration: 3000, fill: 'forwards' }
   )
 }
+
+// Ripple
+
+export function rippleEffect(e) {
+  const x = e.clientX - e.target.offsetLeft
+  const y = e.clientY - e.target.offsetTop
+
+  const span = document.createElement('span')
+  span.style.left = `${x}px`
+  span.style.top = `${y}px`
+
+  console.log(this)
+  this.appendChild(span)
+  setTimeout(() => {
+    span.remove()
+  }, 800)
+}
+
+const button = document.querySelector('.ripple-btn')
+button.addEventListener('click', rippleEffect)
